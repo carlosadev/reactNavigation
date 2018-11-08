0c750715ac93d119c394ce32906392d21cd0e95e
Carlos Andres Acuña <c.andres.a30@gmail.com>
2018-11-08 11:33:16 -0500

    first commit

---
+++ src/screens/login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native';
+import { NavigationActions } from 'react-navigation';
+
+class Login extends Component {
+  handlePress = () => {
+    //this.props.navigation.navigate('About')
+    this.props.navigation.dispatch(
+      NavigationActions.navigate({
+        routeName:'About'
+      })
+    )
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Login   </Text>
+        <Button
+          title="Ir al about"
+          onPress={this.handlePress}
+
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+
+export default Login
---
+++ src/screens/profile.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native';
+
+import Name from '../profile/components/name';
+
+class Profile extends Component {
+  handlePress = () => {
+    this.props.navigation.navigate('Home')
+  }
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: `${navigation.getParam('name')} ${navigation.getParam('age', 27)}`
+    }
+  }
+
+  setParams = () => {
+    this.props.navigation.setParams({
+      name:'Abbiechuella'
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Profile</Text>
+        <Button
+          title="Ir a la home"
+          onPress={this.handlePress}
+        />
+        <Button
+        title="Cambiar nombre"
+        onPress={this.setParams}
+      />
+      <Name  />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+})
+
+export default Profile
